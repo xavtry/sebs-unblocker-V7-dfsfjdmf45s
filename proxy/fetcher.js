c3001620eb2464d712e3b4cfca63c4420b58dce6
xavtry <sebastianishot68@gmail.com>
2025-10-20 17:36:47 -0400

    Implement fetcher function with error handling

--- proxy/fetcher.js
+++ proxy/fetcher.js
@@ -1,1 +1,13 @@
+const fetch = require('node-fetch');
+const config = require('./config');
+const { logger } = require('./logger');
 
+module.exports = async function fetcher(url) {
+  try {
+    const res = await fetch(url, { timeout: config.timeout, headers: { 'User-Agent': config.userAgent }});
+    return await res.text();
+  } catch (err) {
+    logger(`Fetcher error for ${url}: ${err.message}`);
+    throw err;
+  }
+};
