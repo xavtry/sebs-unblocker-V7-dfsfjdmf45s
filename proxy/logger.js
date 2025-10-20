ac1c5293d417c63da4bb8901ce71cbcd180108ba
xavtry <sebastianishot68@gmail.com>
2025-10-20 17:36:37 -0400

    Implement logging functionality in logger.js
    Add a logger function to log messages to console and file

--- proxy/logger.js
+++ proxy/logger.js
@@ -1,1 +1,10 @@
+const fs = require('fs');
+const logPath = './logs/proxy.log';
+
+function logger(msg) {
+  console.log(msg);
+  fs.appendFileSync(logPath, `[${new Date().toISOString()}] ${msg}\n`);
+}
+
+module.exports = { logger };
 
