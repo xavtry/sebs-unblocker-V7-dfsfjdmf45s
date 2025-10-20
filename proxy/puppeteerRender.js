391326b47f01ed52e99c6a0a288494b8e1269f31
xavtry <sebastianishot68@gmail.com>
2025-10-20 17:37:19 -0400

    Add Puppeteer rendering function for URLs

--- proxy/puppeteerRender.js
+++ proxy/puppeteerRender.js
@@ -1,1 +1,12 @@
+const puppeteer = require('puppeteer');
+const config = require('./config');
 
+module.exports = async function(url) {
+  const browser = await puppeteer.launch({ headless: true });
+  const page = await browser.newPage();
+  await page.setUserAgent(config.userAgent);
+  await page.goto(url, { waitUntil: 'networkidle2', timeout: config.timeout });
+  const content = await page.content();
+  await browser.close();
+  return content;
+};
