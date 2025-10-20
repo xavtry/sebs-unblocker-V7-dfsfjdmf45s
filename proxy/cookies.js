d9a508412fa5ba08660a32079608a6483623c7ab
xavtry <sebastianishot68@gmail.com>
2025-10-20 17:52:50 -0400

    Update cookies.js

--- proxy/cookies.js
+++ proxy/cookies.js
@@ -1,1 +1,21 @@
+// basic cookie forwarding helpers
+// This does not implement full cookie jar behavior.
+// It simply forwards incoming cookies from client to origin fetches (where applicable)
+// and copies Set-Cookie headers from origin back to client responses when proxied via /resource.
 
+function attachRequestCookies(fetchOptions, req) {
+  if (!fetchOptions.headers) fetchOptions.headers = {};
+  if (req.headers.cookie) {
+    fetchOptions.headers['cookie'] = req.headers.cookie;
+  }
+}
+
+function forwardSetCookieHeaders(res, originResponse) {
+  const setCookie = originResponse.headers && originResponse.headers.raw && originResponse.headers.raw()['set-cookie'];
+  if (setCookie && setCookie.length) {
+    // forward as-is; Express will handle multiple headers.
+    res.setHeader('Set-Cookie', setCookie);
+  }
+}
+
+module.exports = { attachRequestCookies, forwardSetCookieHeaders };
