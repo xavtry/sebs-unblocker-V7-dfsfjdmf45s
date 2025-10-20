a98b69e86d34d1aeeec84c298779b4770423a59b
xavtry <sebastianishot68@gmail.com>
2025-10-20 17:49:51 -0400

    Update throttler.js

--- proxy/throttler.js
+++ proxy/throttler.js
@@ -1,1 +1,18 @@
+const { RateLimiterMemory } = require('rate-limiter-flexible');
+const config = require('./config');
 
+const rateLimiter = new RateLimiterMemory({
+  points: config.rateLimitPoints,
+  duration: config.rateLimitDuration
+});
+
+async function consumeIp(ip) {
+  try {
+    await rateLimiter.consume(ip);
+    return true;
+  } catch (rej) {
+    return false;
+  }
+}
+
+module.exports = { consumeIp };
