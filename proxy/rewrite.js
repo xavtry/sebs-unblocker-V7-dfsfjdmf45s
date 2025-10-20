3c9083c9ddded32385e5524c9bca378510666ebb
xavtry <sebastianishot68@gmail.com>
2025-10-20 17:37:02 -0400

    Add rewrite function to modify HTML with JSDOM
    Implement HTML rewriting using JSDOM to modify href and src attributes and remove <base> elements.

--- proxy/rewrite.js
+++ proxy/rewrite.js
@@ -1,1 +1,20 @@
+const jsdom = require('jsdom');
+const { JSDOM } = jsdom;
 
+module.exports = async function rewrite(html, baseUrl) {
+  const dom = new JSDOM(html);
+  const document = dom.window.document;
+
+  // Rewrite href/src
+  [...document.querySelectorAll('[href],[src]')].forEach(el => {
+    const attr = el.href ? 'href' : 'src';
+    try {
+      el[attr] = `/proxy?url=${encodeURIComponent(new URL(el[attr], baseUrl))}`;
+    } catch {}
+  });
+
+  // Remove <base>
+  [...document.querySelectorAll('base')].forEach(b => b.remove());
+
+  return dom.serialize();
+};
