41de70d714198122b51077f9019ac4b25c257a23
xavtry <sebastianishot68@gmail.com>
2025-10-20 17:39:20 -0400

    Refactor setup.js to create new directories and log file

--- setup.js
+++ setup.js
@@ -1,11 +1,6 @@
-
 const fs = require('fs');
-const path = require('path');
-
-const dirs = ['logs', 'public/css', 'public/js', 'public/fonts'];
-
-dirs.forEach(dir => {
-  if (!fs.existsSync(dir)) fs.mkdirSync(dir, { recursive: true });
-});
-
-console.log('Setup complete: folders created.');
+const paths = ['logs','proxy','public/js','public/css','views'];
+paths.forEach(p => { if (!fs.existsSync(p)) fs.mkdirSync(p, { recursive: true }); });
+const log = 'logs/proxy.log';
+if (!fs.existsSync(log)) fs.writeFileSync(log, '');
+console.log('Setup done.');
