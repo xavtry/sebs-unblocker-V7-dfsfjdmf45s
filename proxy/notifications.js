cb0b088df75ca1d72707007b1fb04b754c9d14a3
xavtry <sebastianishot68@gmail.com>
2025-10-20 17:53:28 -0400

    Update notifications.js

--- proxy/notifications.js
+++ proxy/notifications.js
@@ -1,1 +1,10 @@
 
+// basic notification stub - prints to console. Extend to send email, Slack, etc.
+const { logger } = require('./logger');
+
+function notifyAdmin(subject, body) {
+  // In production, hook this up to SMTP / Sendgrid / Slack / PagerDuty.
+  logger(`[ALERT] ${subject} - ${body}`);
+}
+
+module.exports = { notifyAdmin };
