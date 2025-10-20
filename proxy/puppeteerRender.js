d9cb70deb257da7ee766b4df72a8ee5bb6c1db7d
xavtry <sebastianishot68@gmail.com>
2025-10-20 17:42:52 -0400

    Refactor puppeteerRender.js by removing function
    Removed the existing module export function for rendering a URL with Puppeteer and added imports for 'rewriteHtml' and 'log'.

--- proxy/puppeteerRender.js
+++ proxy/puppeteerRender.js
@@ -1,12 +1,4 @@
 const puppeteer = require('puppeteer');
 const config = require('./config');
-
-module.exports = async function(url) {
-  const browser = await puppeteer.launch({ headless: true });
-  const page = await browser.newPage();
-  await page.setUserAgent(config.userAgent);
-  await page.goto(url, { waitUntil: 'networkidle2', timeout: config.timeout });
-  const content = await page.content();
-  await browser.close();
-  return content;
-};
+const { rewriteHtml } = require('./htmlParser');
+const { log }
