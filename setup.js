726457aaca403b9321e3f1f3f05b4b96a924ba35
xavtry <sebastianishot68@gmail.com>
2025-10-20 17:35:42 -0400

    Create necessary directories during setup
    Added directory creation for logs and public assets.

--- setup.js
+++ setup.js
@@ -1,1 +1,11 @@
 
+const fs = require('fs');
+const path = require('path');
+
+const dirs = ['logs', 'public/css', 'public/js', 'public/fonts'];
+
+dirs.forEach(dir => {
+  if (!fs.existsSync(dir)) fs.mkdirSync(dir, { recursive: true });
+});
+
+console.log('Setup complete: folders created.');
