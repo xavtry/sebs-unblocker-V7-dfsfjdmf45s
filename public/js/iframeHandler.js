a326cfd0cf9973b8a32e576ba68bed3905c61aea
xavtry <sebastianishot68@gmail.com>
2025-10-20 17:55:06 -0400

    Update iframeHandler.js

--- public/js/iframeHandler.js
+++ public/js/iframeHandler.js
@@ -1,1 +1,8 @@
 
+// small helpers for iframe interactions (postMessage pass-through)
+// Minimal for now; can be expanded
+window.addEventListener('message', (ev) => {
+  // For security, you could inspect ev.origin and ev.data
+  // Currently just log
+  console.log('iframe message', ev.origin, ev.data);
+});
