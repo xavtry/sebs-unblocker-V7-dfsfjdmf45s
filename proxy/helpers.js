6cd8049fee7894f40853db39f3aa2baf029364ca
xavtry <sebastianishot68@gmail.com>
2025-10-20 17:41:01 -0400

    Add safeUrl and sameOrigin functions to helpers

--- proxy/helpers.js
+++ proxy/helpers.js
@@ -1,1 +1,5 @@
-
+function safeUrl(u){
+  try { return new URL(u).href; } catch { return null; }
+}
+function sameOrigin(a,b){ try { return new URL(a).origin === new URL(b).origin; } catch { return false; } }
+module.exports = { safeUrl, sameOrigin };
