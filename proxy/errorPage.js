9de7093a5747d8e206488ae25a545e80f7f76842
xavtry <sebastianishot68@gmail.com>
2025-10-20 17:57:00 -0400

    Update errorPage.js

--- proxy/errorPage.js
+++ proxy/errorPage.js
@@ -1,1 +1,14 @@
+function renderError(res, msg = 'An error occurred') {
+  res.status(500).send(`
+    <html>
+      <head><title>Error</title></head>
+      <body style="background:#010a0a;color:#ff5555;font-family:sans-serif;">
+        <h1>Error</h1>
+        <p>${msg}</p>
+      </body>
+    </html>
+  `);
+}
+
+module.exports = { renderError };
 
