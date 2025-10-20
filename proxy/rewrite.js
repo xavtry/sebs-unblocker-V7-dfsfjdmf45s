edf59a00c7eaf44a107465a35adcaa6fe23ac789
xavtry <sebastianishot68@gmail.com>
2025-10-20 17:42:27 -0400

    Refactor rewrite function to use htmlParser

--- proxy/rewrite.js
+++ proxy/rewrite.js
@@ -1,20 +1,4 @@
-const jsdom = require('jsdom');
-const { JSDOM } = jsdom;
-
-module.exports = async function rewrite(html, baseUrl) {
-  const dom = new JSDOM(html);
-  const document = dom.window.document;
-
-  // Rewrite href/src
-  [...document.querySelectorAll('[href],[src]')].forEach(el => {
-    const attr = el.href ? 'href' : 'src';
-    try {
-      el[attr] = `/proxy?url=${encodeURIComponent(new URL(el[attr], baseUrl))}`;
-    } catch {}
-  });
-
-  // Remove <base>
-  [...document.querySelectorAll('base')].forEach(b => b.remove());
-
-  return dom.serialize();
+const { rewriteHtml } = require('./htmlParser');
+module.exports = async function rewrite(html, baseUrl){
+  return rewriteHtml(html, baseUrl);
 };
